62194c76eef98fea6de03f1261f0ec3b0177470b
shashikantb <borgavakarshashikant@gmail.com>
2025-04-12 13:22:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -118,4 +118,5 @@ def view_ideas():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host='0.0.0.0', port=port)
